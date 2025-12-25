dce25f0bed42cf9098ba441de08c42fea60a11b5
Cheng Shao <terrorjack@type.dance>
2025-12-25 06:45:58 +0100

    wasm: use Text in decodeResults for better efficiency

--- src-wasm/Language/Javascript/JSaddle/Wasm/Internal.hs
+++ src-wasm/Language/Javascript/JSaddle/Wasm/Internal.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Language.Javascript.JSaddle.Wasm.Internal
@@ -12,10 +13,10 @@ import Control.Exception (evaluate)
 import Control.Monad ((<=<), (>=>))
 import Data.Aeson qualified as A
 import Data.ByteString (ByteString)
-import Data.ByteString.Internal qualified as BI
 import Data.ByteString.Lazy.Char8 qualified as BLC8
 import Data.ByteString.Unsafe qualified as BU
-import Foreign.Ptr (Ptr)
+import Data.Primitive
+import Data.Text.Internal qualified as T
 import GHC.Wasm.Prim (JSString, JSVal)
 import GHC.Wasm.Prim qualified
 import Language.Javascript.JSaddle.Run (runJavaScript)
@@ -107,11 +108,7 @@ runHelper entryPoint sendOutgoingMessage onIncomingMessage = do
     encodeBatch = byteStringToJSString . BLC8.toStrict . A.encode
 
     decodeResults :: JSString -> IO Results
-    decodeResults s = do
-      bs <- jsStringToByteString s
-      case A.eitherDecodeStrict bs of
-        Left e -> fail $ "jsaddle: received invalid JSON: " <> show e
-        Right r -> pure r
+    decodeResults = A.throwDecodeStrictText <=< jsStringToText
 
 -- Utilities:
 
@@ -132,21 +129,23 @@ foreign import javascript safe "$1.addEventListener('message', e => {\
   \})"
   js_onMessage :: JSVal -> JSVal -> IO ()
 
--- Conversion JSString <-> ByteString
+-- Conversion JSString <-> ByteString/Text
 
 foreign import javascript unsafe "$1.length"
   js_stringLength :: JSString -> IO Int
 
 foreign import javascript unsafe "(new TextEncoder()).encodeInto($1, new Uint8Array(__exports.memory.buffer, $2, $3)).written"
-  js_encodeInto :: JSString -> Ptr a -> Int -> IO Int
+  js_encodeInto ::
+    JSString -> MutableByteArray# s -> Int -> IO Int
 
-jsStringToByteString :: JSString -> IO ByteString
-jsStringToByteString s = do
-  len <- js_stringLength s
-  -- see https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder/encodeInto#buffer_sizing
-  -- (could also use another strategy described there)
-  let lenMax = len * 3
-  BI.createUptoN lenMax \buf -> js_encodeInto s buf lenMax
+jsStringToText :: JSString -> IO T.Text
+jsStringToText s = do
+  len_max <- (* 3) <$> js_stringLength s
+  mba@(MutableByteArray mba#) <- newByteArray len_max
+  len <- js_encodeInto s mba# len_max
+  shrinkMutableByteArray mba len
+  ba <- unsafeFreezeByteArray mba
+  pure $ T.Text ba 0 len
 
 foreign import javascript unsafe "(new TextDecoder('utf-8', {fatal: true})).decode(new Uint8Array(__exports.memory.buffer, $1, $2))"
   js_toJSString :: Ptr a -> Int -> IO JSString
